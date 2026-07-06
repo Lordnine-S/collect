ebad034ba1230ee9f88a4092159b85ec68358971
Lordnine-S <javachip1201@gmail.com>
2026-07-06 22:23:03 +0900

    Update supabaseClient.js

--- src/core/supabaseClient.js
+++ src/core/supabaseClient.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://cvuslommhbzjjoltlfkk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_-caYxifFCxM2b46wEZ-x9g_uJT3-TUP";
-const BOSS_SUPABASE_URL = "https://jxmudwscinjmyuchecav.supabase.co";
-const BOSS_SUPABASE_ANON_KEY = "sb_publishable_Pnm--yk56qY58UCPfrAGRQ_ambbNj3o";
+const SUPABASE_URL = "https://hutswcrqtbkbuwasifum.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_BOgoRPamrKzC2dJfgwgTrQ_xxB0yT4a";
+const BOSS_SUPABASE_URL = "https://ttgnoqehlupllpfyhuff.supabase.co";
+const BOSS_SUPABASE_ANON_KEY = "sb_publishable_nVsvYqo3fjX_RHQO2in5XA_F9J0bM27";
 
 export const APP_SETTINGS_TABLE = "app_settings";
 export const ADMIN_PASSWORD_KEY = "admin_password";
